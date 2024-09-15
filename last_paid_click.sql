1a7f8bd0805de0033dd5d1a32e27d89d8668cd7f
Fominochek <168715222+Fominochek@users.noreply.github.com>
2024-09-15 15:53:07 +0300

    Add files via upload

--- last_paid_click.sql
+++ last_paid_click.sql
@@ -1,43 +1,44 @@
 --2.
-with l_s as(
-	select 
-		s.visitor_id,
-		MAX(s.visit_date) as visit_date,
-		s.source as utm_source,
-		s.medium as utm_medium,
-		s.campaign as utm_campaign,
-		l.lead_id,
-		l.created_at,
-		l.amount,
-		l.closing_reason,
-		l.status_id
-	from leads l 
-	left join sessions s
-	on l.visitor_id = s.visitor_id
-	group by 1, 3, 4, 5, 6, 7, 8, 9, 10
-	order by visitor_id asc, visit_date desc
+WITH PaidClicks AS (
+  SELECT 
+    visitor_id,
+    visit_date,
+    "source",
+    medium,
+    campaign
+  FROM sessions
+  where medium IN ('cpc', 'cpm', 'cpa', 'youtube', 'cpp', 'tg', 'social')
 ),
-	l_s1 as (
-    	select distinct on (visitor_id) *
-    	from l_s
-    	where utm_medium != 'organic'
+LastPaidClicks AS (
+  SELECT 
+    visitor_id,
+    MAX(visit_date) AS last_paid_click_date,
+    "source",
+    medium,
+    campaign
+  FROM PaidClicks
+  GROUP BY visitor_id, "source", medium, campaign
 )
-select
-	visitor_id,
-	visit_date, 
-	utm_source,
-    utm_medium,
-    utm_campaign,
-    lead_id,
-    created_at,
-    amount,
-    closing_reason,
-    status_id
-from l_s1
-order by
-    amount desc nulls last,
-    visit_date asc,
-    utm_source asc,
-    utm_medium asc,
-    utm_campaign asc
-limit 10;
+SELECT 
+  l.visitor_id,
+  l.visit_date,
+  l."source",
+  l.medium,
+  l.campaign,
+  lead.lead_id,
+  lead.created_at,
+  lead.amount,
+  lead.closing_reason,
+  lead.status_id
+FROM leads AS lead
+JOIN sessions AS l
+  ON lead.visitor_id = l.visitor_id
+LEFT JOIN LastPaidClicks AS last_click
+  ON l.visitor_id = last_click.visitor_id AND l.visit_date = last_click.last_paid_click_date
+ORDER BY
+  lead.amount DESC NULLS LAST,
+  l.visit_date ASC,
+  l."source" ASC,
+  l.medium ASC,
+  l.campaign ASC
+  limit 10;
