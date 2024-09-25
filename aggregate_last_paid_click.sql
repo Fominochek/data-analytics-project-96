f03efbf3e14bba067e252dc6f73fdfa619ee4991
Fominochek <168715222+Fominochek@users.noreply.github.com>
2024-09-25 21:10:30 +0300

    Add files via upload

--- aggregate_last_paid_click.sql
+++ aggregate_last_paid_click.sql
@@ -1,66 +1,69 @@
 --3.
 WITH lv AS (
-   select
-      MAX(visit_date) AS max_visit_date,
-      visitor_id
-   from sessions
-   where medium != 'organic'
-   group by visitor_id
+    SELECT
+        MAX(visit_date) AS max_visit_date,
+	visitor_id
+    FROM sessions
+    WHERE medium != 'organic'
+    GROUP BY visitor_id
 ),
 
 leads AS (
-   select
-      DATE(lv.max_visit_date) AS visit_date,
-      s.source AS utm_source,
-      s.medium AS utm_medium,
-      s.campaign AS utm_campaign,
-      COUNT(DISTINCT lv.visitor_id) AS visitors_count,
-      COUNT(l.lead_id) AS leads_count,
-      COUNT(case
-                when l.status_id = 142 OR l.closing_reason = 'Успешно реализовано' 
-                then lv.visitor_id 
-                end) AS purchases_count,
-      SUM(l.amount) AS revenue
-   from lv
-   inner join sessions AS s
-      on lv.visitor_id = s.visitor_id AND lv.max_visit_date = s.visit_date
-   left join leads AS l
-      on s.visitor_id = l.visitor_id AND l.created_at >= lv.max_visit_date
-   group by visit_date, utm_source, utm_medium, utm_campaign
+    SELECT
+        DATE(lv.max_visit_date) AS visit_date,
+	s.source AS utm_source,
+	s.medium AS utm_medium,
+	s.campaign AS utm_campaign,
+	COUNT(DISTINCT lv.visitor_id) AS visitors_count,
+	COUNT(l.lead_id) AS leads_count,
+	COUNT(CASE
+            WHEN l.status_id = 142
+            OR l.closing_reason = 'Успешно реализовано'
+            THEN lv.visitor_id
+            END) AS purchases_count,
+        SUM(l.amount) AS revenue
+    FROM lv
+    INNER JOIN sessions AS s
+        ON lv.visitor_id = s.visitor_id AND lv.max_visit_date = s.visit_date
+    LEFT JOIN leads AS l
+        ON s.visitor_id = l.visitor_id AND l.created_at >= lv.max_visit_date
+    GROUP BY visit_date, utm_source, utm_medium, utm_campaign
 ),
 
 ads AS (
-   select
-      DATE(campaign_date) AS campaign_date,
-      utm_source,
-      utm_medium,
-      utm_campaign,
-      SUM(daily_spent) AS total_cost
-   from (
-      select * 
-      from vk_ads
+    SELECT
+        DATE(campaign_date) AS campaign_date,
+        utm_source,
+        utm_medium,
+        utm_campaign,
+        SUM(daily_spent) AS total_cost
+    FROM (
+      SELECT * 
+      FROM vk_ads
       UNION ALL
-      select * 
-      from ya_ads
+      SELECT * 
+      FROM ya_ads
     ) AS ads
-   group by campaign_date, utm_source, utm_medium, utm_campaign
+    GROUP BY campaign_date, utm_source, utm_medium, utm_campaign
 )
 
-select
-   l.visit_date,
-   l.visitors_count,
-   l.utm_source,
-   l.utm_medium,
-   l.utm_campaign,
-   a.total_cost,
-   l.leads_count,
-   l.purchases_count,
-   l.revenue
-from leads AS l
-left join ads as a
-   on l.utm_source = a.utm_source
-   and l.utm_medium = a.utm_medium
-   and l.utm_campaign = a.utm_campaign
-   and l.visit_date = a.campaign_date
-order by l.revenue DESC NULLS LAST, l.visit_date ASC, l.visitors_count DESC, l.utm_source ASC, l.utm_medium ASC, l.utm_campaign ASC
+SELECT
+    l.visit_date,
+    l.visitors_count,
+    l.utm_source,
+    l.utm_medium,
+    l.utm_campaign,
+    a.total_cost,
+    l.leads_count,
+    l.purchases_count,
+    l.revenue
+FROM leads AS l
+LEFT JOIN ads AS a
+   ON l.utm_source = a.utm_source
+   AND l.utm_medium = a.utm_medium
+   AND l.utm_campaign = a.utm_campaign
+   AND l.visit_date = a.campaign_date
+ORDER BY l.revenue DESC NULLS LAST, l.visit_date ASC,
+    l.visitors_count DESC, l.utm_source ASC,
+    l.utm_medium ASC, l.utm_campaign ASC
 LIMIT 15;
